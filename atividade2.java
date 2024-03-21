b3f8edba23eb53b3131a500f4190a6f0f50cd5f4
Igor-monteiro-barreto <igormonteirobarreto047@gmail.com>
2024-03-21 13:03:11 -0300

    primeira atividade

---
+++ atividade2.java
@@ -0,0 +1,8 @@
+public class atividade2 {
+    public static void main(String[] args){
+        String nome = "José";
+        int idade = 30 ;
+        double peso = 55.60 ;
+        System.out.printf("%s , %d anos, pesa: %2f kg", idade , nome, peso);
+    }
+}
---
+++ exemplo1.java
@@ -0,0 +1,8 @@
+public class exemplo1 {
+    public static void main(String[] args) {
+        int soma = 0 ;
+        for(int i = 1; i < 6 ; i++ ){
+            soma += 1 ;
+        }
+    }
+}

44f9ea6fd21fed83fc7a0321d47b42633fece021
Igor-monteiro-barreto <igormonteirobarreto047@gmail.com>
2024-04-22 12:50:16 -0300

    Array inicio

---
+++ atvid4.java
@@ -0,0 +1,14 @@
+public class atvid4 {
+	public static void main (String[] args) {
+		int[] vet = new int [5];
+		vet[0] = 5;
+		vet[1] = 3;
+		vet[2] = 8;
+		vet[3] = 6;
+		vet[4] = 9;
+		
+		for(int i = 0; i < vet.length; i++) {
+			System.out.println("O vetor é [" + i + "] = " + vet[i]);
+		}
+	}
+}
